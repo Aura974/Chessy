b3c416222f62dcc19e1bbfb943384e7bd6941227
Aura <aurore.ducap@gmail.com>
2021-04-10 15:36:15 +0400

    Pseudo-code class Player

---
+++ Controller/match.py
@@ -0,0 +1,9 @@
+class Match:
+    def __init__(self):
+        self.player_one = list()
+        self.player_two = list()
+
+    def player_one(self):
+        player_one = Player(name, surname)
+        
+
