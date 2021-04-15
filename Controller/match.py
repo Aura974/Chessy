06aae5024d556f89c3bfadd3ba7903bb80c6a87b
Aura <aurore.ducap@gmail.com>
2021-04-15 14:41:28 +0400

    deleting ancient files

--- Controller/match.py
+++
@@ -1,9 +0,0 @@
-class Match:
-    def __init__(self):
-        self.player_one = list()
-        self.player_two = list()
-
-    def player_one(self):
-        player_one = Player(name, surname)
-        
-
