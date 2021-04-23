23d98a29bf8c16ab4f182463232683cb6ba6cbf9
Aura <aurore.ducap@gmail.com>
2021-04-23 14:57:54 +0400

    test dictionnaire scores

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from controller.tournament import TournamentController
 if __name__ == "__main__":
     tournamentController = TournamentController()
     tournamentController.run_first_round()
-    tournamentController.run_other_rounds()
+    # tournamentController.run_other_rounds()
