c23276b9197e0cd67911495feb3fe659ae293183
Aura <aurore.ducap@gmail.com>
2025-01-13 14:39:22 +0400

    New version - 2025

--- main.py
+++
@@ -1,5 +0,0 @@
-from controller.menu_controller import home_menu
-
-
-if __name__ == "__main__":
-    home_menu()
--- model/match.py
+++
@@ -1,6 +0,0 @@
-class Match:
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-        self.score_player1 = 0
-        self.score_player2 = 0
